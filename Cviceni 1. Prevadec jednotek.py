cad4118d9e49737af3a82bed7e6c8bdb3edb3b2c
RadimJedlicka <radim.jedi@gmail.com>
2021-12-07 16:34:50 +0100

    uploaded Cviceni 1. Prevadec jednotek.py

---
+++ Cviceni 1. Prevadec jednotek.py
@@ -0,0 +1,19 @@
+# Prevodni pomery
+kg_lb = 2.20
+km_mile = 0.62
+l_gal = 0.26
+
+# Pocet jednotek, ktery ma byt preveden.
+kg_pocet = 80
+km_pocet = 54
+l_pocet = 5
+
+# Vypocty pro prevod
+kg_vysledek = kg_pocet * kg_lb
+km_vysledek = km_pocet * km_mile
+l_vysledek = l_pocet * l_gal
+
+# Vysledne odpovedi
+print(kg_pocet, "kilogramu se rovna", kg_vysledek, "liber.")
+print(km_pocet, "kilometru se rovna", km_vysledek, "mil.")
+print(l_pocet, "litru se rovna", l_vysledek, "galonu.")
