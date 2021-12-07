3f2c8c3f40ff376df7f990bc630691e134d32c5e
RadimJedlicka <radim.jedi@gmail.com>
2021-12-07 17:07:03 +0100

    uploaded Cviceni 1. Prevadec jednotek.py
    also uploaded Cviceni 2. Nakupujeme auto.py

--- Cviceni 01. Prevadec jednotek.py
+++ Cviceni 01. Prevadec jednotek.py
@@ -17,4 +17,4 @@ l_vysledek = l_pocet * l_gal
 print(kg_pocet, "kilogramu se rovna", kg_vysledek, "liber.")
 print(km_pocet, "kilometru se rovna", km_vysledek, "mil.")
 print(l_pocet, "litru se rovna", l_vysledek, "galonu.")
-print("Uz tu umim docela dost veci"
+print("Uz tu umim docela dost veci")
