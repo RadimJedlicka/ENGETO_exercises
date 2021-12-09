092638f598556387c3989cec38b77c3c03382da4
RadimJedlicka <radim.jedi@gmail.com>
2021-12-09 15:15:29 +0100

    dalsi cviceni

---
+++ Cviceni 32. Nejdelsi slovo.py
@@ -0,0 +1,31 @@
+# Napiš program, který z listu slova vybere nejdelší slovo a vytiskne do terminálu
+# tuple s tímto nejdelším slovem a jeho délkou.
+#
+# Použij prosím tento list:
+
+slova = [
+'Python', 'is', 'a', 'widely', 'used',
+'high-level', 'programming', 'language',
+'for', 'general-purpose', 'programming,',
+'created', 'by', 'Guido', 'van', 'Rossum',
+'and', 'first', 'released', 'in', '1991.'
+]
+
+# moje reseni
+# list = []
+# for slovo in slova:
+#     list.append((len(slovo), slovo))
+#
+# vysledek = (sorted(list, reverse=True))
+#
+# print(vysledek[0][1],vysledek[0][0])
+
+# reseni pomoci while cyklu
+nejdelsi_slovo  = ('', 0)
+
+while slova:
+    slovo = slova.pop(0)
+    if len(slovo) > nejdelsi_slovo[1]:
+        nejdelsi_slovo = (slovo, len(slovo))
+
+print(nejdelsi_slovo)
