a1b24a02654439fb4f03f9c4261af5b3076d0455
RadimJedlicka <radim.jedi@gmail.com>
2022-01-16 19:47:45 +0100

    Cviceni_48_Caesarova_sifra.py se zadanim

--- Cviceni_48_Caesarova_sifra.py
+++ Cviceni_48_Caesarova_sifra.py
@@ -1,3 +1,20 @@
+# Caesarova šifra je typ nahrazovací šifry, ve které je každé písmeno nahrazeno písmenem,
+# které se abecedně nachází o pevně určený počet míst dále. Tato metoda šifrování se jmenuje po Juliu Caesarovi,
+# který šifrování používal ve své osobní korespondenci.
+
+# Ceasarova šifra v praxi:
+
+# S posunem o 3 DOLEVA (-3) by písmeno D bylo nahrazeno písmenem A, E by bylo nahrazeno B, atd.
+# S posunem o 3 DOPRAVA (3) by písmeno D bylo nahrazeno písmenem G, E by bylo nahrazeno H, atd.
+
+# Příklad fungující funkce:
+# >>> message = 'abc def ghi jkl mno pqr stu vwx Yz'
+# >>> caesar(message,2)
+# cde fgh ijk lmn opq rst uvw xyz Ab
+# >>> caesar(message,-2)
+# yza bcd efg hij klm nop qrs tuv Wx
+# --------------------------------------------------
+
 # Zprava
 message = 'abc def ghi jkl mno pqr stu vwx Yz'
 shift = -1
