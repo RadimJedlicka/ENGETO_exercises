25796e38edc4b54460d5f2d5eae9c8729c22496b
radim <radim.jedi@gmail.com>
2022-04-01 12:11:18 +0200

    komplexni cviceni

--- Cviceni_X_Komplexni-ukol-na-metody-stringu.py
+++ Cviceni_X_Komplexni-ukol-na-metody-stringu.py
@@ -1,3 +1,15 @@
+# Nyní dostaneš zadání úkolů, které prověří tvé porozuměnní novým metodám. Dej si na ně čas a ber jeden úkol po druhém. Pokud nevíš, neváhej se podívat do nápovědy nebo rovnou otevři řešení. Pojďme na to:
+#
+# TODO Na řádku 5 do proměnné rozdeleny_string ulož muj_string jako list rozdělený podle mezer.
+# TODO Na řádku 12 a 13 do proměnné emaily přidej pouze emailové adresy pomocí jejich indexu v proměnné rozdeleny_string.
+# TODO Na řádku 19 a 20 získej pouze domény (text za znakem '@') z emailů v listu emaily. První doménu ulož do proměnné domena01 a druhou do domena02.
+# TODO Na řádku 23 a 24 rozděl stringy v promměnných domena01 a domena02 podle znaku '.' a ulož pouze první prvek (na indexu 0) do stejné proměnné.
+# TODO Na řádku 29 ověř pomocí podmínky if, zda proměnná domena01 neobsahuje žádná čísla.
+# TODO Na řádku 30 přiřaď kód pod příkaz if, který spojí s prázdným stringem bez_cisel pouze tu doménu, která neobsahuje číslice.
+# TODO Na řádku 31 přiřaď pod příkaz if funkci print(), která vypíše následující: 'Doména', <doména>, 'byla přidána.'.
+# TODO Na řádku 33 přiřaď pod příkaz else funkci print(), která vypíše následující: 'Doména <doména> nebyla přidána, protože obsahuje čísla!'.
+# TODO To samé proveď na řádku 36, 37, 38 (if), a 40 (else) proměnnou domena02.
+
 # Zadany string
 muj_string = 'Abc@abc.cz a Matous@1234.cz jsou naše emailové adresy'
 
